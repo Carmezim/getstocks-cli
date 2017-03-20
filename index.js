07d0d120196ed37a57616f5b020660410fe649dc
Carmezim <carmezim.filho@gmail.com>
2017-03-20 07:33:58 +0100

    cli

--- index.js
+++ index.js
@@ -1,22 +1,21 @@
-"use strict"
+#!/usr/bin/env node
 
 const req = require('request');
+const app = require('commander');
 
 const URL = 'https://finance.google.com/finance/info?&q=';
-var stocks = ["KO", "BRK.A"]
 
-const stocksToGet = stocks.join(',');
-
-console.log(stocksToGet);
-
-function get_prices(){
-  req(URL + stocksToGet, function (err, response, stockInfo) {
-    if (!err && response.statusCode == 200) {
-      stockInfo = JSON.parse(stockInfo.replace(/\/+/g, ''))
+app
+  .arguments('<stock-ticker>')
+  .action(function(stocksToGet){
+    req(URL + stocksToGet, function (err, response, stockInfo) {
+      if (!err && response.statusCode == 200) {
+        stockInfo = JSON.parse(stockInfo.replace(/\/+/g, ''))
         console.log(stockInfo)
-    } else {
-      throw err;
-    }  
-  })
-}
-setInterval(get_prices, 1000);
+      } else {
+        throw err;
+      }
+    });
+  });
+
+app.parse(process.argv);
